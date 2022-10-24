9ca7a658e9e35f461c3e37f7b6fff6a91cfea06e
LamplightScald <Lamplight371@gmail.comw>
2022-10-24 17:38:16 -0400

    Initial Commit

---
+++ Handbook/HandbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  HandbookApp.swift
+//  Handbook
+//
+//  Created by Lamplight on 10/24/22.
+//
+
+import SwiftUI
+
+@main
+struct HandbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
